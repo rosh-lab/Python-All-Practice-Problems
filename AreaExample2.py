5405930a34fb41b3632f36abacbb95a854ba1ebc
rosh-lab <roshanaara2318@gmail.com>
2026-03-27 09:45:40 +0530

    Initial commit

---
+++ AreaExample2.py
@@ -0,0 +1,6 @@
+#Program which will calculate area and perimeter of square
+side=float(input("Enter side:"))
+sa=side*side
+sp=4*side
+print("Area of square= {}".format(sa))
+print("Perimeter= {}".format(sp))
