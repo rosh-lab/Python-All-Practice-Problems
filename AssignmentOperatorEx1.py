e4e869c8cc02b3a7d88b8bf72ea1628148e22896
rosh-lab <roshanaara2318@gmail.com>
2026-04-04 09:46:02 +0530

    Examples

---
+++ AssignmentOperatorEx1.py
@@ -0,0 +1,13 @@
+#Multi-line assignment ,swapping logic
+a,b=input("Enter value of a:"),input("Enter value of b:")
+print("*"*50)
+print("Original value of a {}".format(a))
+print("Original value of b {}".format(b))
+print("*"*50)
+#swapping logic
+a,b=b,a#multi line assignment...
+#k=a single line assignment
+#a=b
+#b=k
+print("Swapped value of {}".format(a))
+print("Swapped value of {}".format(b))
